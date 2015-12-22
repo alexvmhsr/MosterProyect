622e6ca815953f27e6cfbb3effc39415c3a98d77
dennysq <dennysaurio@gmail.com>
2015-12-21 23:18:56 -0500

    primera implementacion de un menu

---
+++ excursion-web/src/main/java/com/teamj/distribuidas/web/MenuBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teamj.distribuidas.web;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Dennys
+ */
+@SessionScoped
+@ManagedBean
+public class MenuBean {
+
+    private String perfil;
+    private String excursiones;
+    private String mochila;
+    private String comentarios;
+    
+
+}
